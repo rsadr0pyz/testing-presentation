bbc82f98874e15ac254d601be0d0deaf3ed87341
rsadr0pyz <rsadr0pyz@gmail.com>
2024-08-22 15:52:57 +0000

    Teste de Integracao

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,7 @@ import { AppComponent } from './app.component';
 import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { MockedCounterComponent } from "./spec-helpers/counter.mock.component"
+import { CounterComponent } from './counter/counter.component';
 
 describe('AppComponent', () => {
 
@@ -40,3 +41,44 @@ describe('AppComponent', () => {
     })
 
 });
+
+
+describe("AppComponent Integration", () => {
+    let fixture : ComponentFixture<AppComponent>
+    let counterEl : DebugElement
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [AppComponent, CounterComponent], //Componente Real
+            schemas: [NO_ERRORS_SCHEMA]
+        }).compileComponents()
+
+        fixture = TestBed.createComponent(AppComponent)
+        fixture.detectChanges()
+        counterEl = fixture.debugElement.query(By.directive(MockedCounterComponent))
+    })
+
+    it("Displays the correct amount of buttons clicks", () => {
+        let el = fixture.debugElement.query(By.css(`[data-testid="button-clicked-amount"]`))
+        let buttons = fixture.debugElement.queryAll(By.css("button"))
+
+        let buttonsClicked = 0;
+        for(let btt of buttons){
+            clickIn(btt)
+            buttonsClicked++
+        }
+
+        fixture.detectChanges()
+
+        expect(el.nativeElement.innerHTML).toEqual(`${buttonsClicked}`) 
+    })
+
+
+})
+
+
+function clickIn(element : DebugElement, ignoreDisable = false){
+    expect(element).toBeTruthy()
+    if(ignoreDisable || !element.properties['disabled'])
+        element.triggerEventHandler("click", null)
+}
